45584ca539d796552a7ea731fac8e99d2879a03f
Suchocka-Klaudia <klaudiasuchocka@vp.pl>
2019-01-17 18:43:01 +0100

    init GUIGameView

--- src/towerdefense/view/GUIGameView.java
+++ src/towerdefense/view/GUIGameView.java
@@ -1,10 +1,16 @@
 package towerdefense.view;
 
+import javafx.stage.Screen;
 import towerdefense.document.Document;
 
+import javax.swing.*;
+
 public class GUIGameView extends GameView {
+    private JPanel panel;
+
     public GUIGameView(Document document) {
         super(document, "GUI");
+
     }
 
     @Override
@@ -24,6 +30,12 @@ public class GUIGameView extends GameView {
 
     @Override
     protected void displayWindow() {
-
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                window.setContentPane(panel);
+                window.setVisible(true);
+            }
+        });
     }
 }
