c36e2f37e8cf38c9bc0aa16807218f6513daec4c
Suchocka-Klaudia <klaudiasuchocka@vp.pl>
2019-01-18 12:11:00 +0100

    GUIGameView set Image Buttons

--- src/towerdefense/view/GUIGameView.java
+++ src/towerdefense/view/GUIGameView.java
@@ -5,7 +5,12 @@ import towerdefense.document.Document;
 import towerdefense.document.Tower;
 import towerdefense.document.towers.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +24,7 @@ public class GUIGameView extends GameView {
     private JButton button6;
     private JButton button7;
     private JButton button8;
+    private Image i1, i2, i3, i4, i5, i6, i7, i8;
     private JPanel mainPanel;
     private JLabel towersLabel;
     private JPanel mapPanel;
@@ -26,25 +32,42 @@ public class GUIGameView extends GameView {
     private JLabel moneyLabel;
     private JLabel playerNameLabel;
     private JLabel gameGoalLabel;
-    private List<Tower> towers;
 
     public GUIGameView(Document document) {
         super(document, "GUI");
         currentPlayer = document.getCurrentPlayer();
-        towers = new ArrayList<>();
-        towers.add(new ArcherTower());
-        towers.add(new EarthTower());
-        towers.add(new ElectricTower());
-        towers.add(new FireTower());
-        towers.add(new ForceTower());
-        towers.add(new IceTower());
-        towers.add(new NuclearTower());
-        towers.add(new WaterTower());
     }
 
     @Override
     protected void displayBoughtTowers() {
-        //na każdy button ustawić obrazek z data/towersPNG
+        try {
+            i1 = ImageIO.read(new File("data/towersPNG/archer.png"));
+            button1.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button1.setIcon(new ImageIcon(i1));
+            i2 = ImageIO.read(new File("data/towersPNG/earth.png"));
+            button2.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button2.setIcon(new ImageIcon(i2));
+            i3 = ImageIO.read(new File("data/towersPNG/electric.png"));
+            button3.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button3.setIcon(new ImageIcon(i3));
+            i4 = ImageIO.read(new File("data/towersPNG/fire.png"));
+            button4.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button4.setIcon(new ImageIcon(i4));
+            i5 = ImageIO.read(new File("data/towersPNG/force.png"));
+            button5.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button5.setIcon(new ImageIcon(i5));
+            i6 = ImageIO.read(new File("data/towersPNG/ice.png"));
+            button6.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button6.setIcon(new ImageIcon(i6));
+            i7 = ImageIO.read(new File("data/towersPNG/nuclear.png"));
+            button7.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button7.setIcon(new ImageIcon(i7));
+            i8 = ImageIO.read(new File("data/towersPNG/water.png"));
+            button8.setBorder(BorderFactory.createEmptyBorder(4, 12, 2, 0));
+            button8.setIcon(new ImageIcon(i8));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
@@ -83,4 +106,5 @@ public class GUIGameView extends GameView {
     {
         return "<html>" + orig.replaceAll("\n", "<br>");
     }
+
 }
